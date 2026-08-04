30786cacb85c64559de6c77e0bbbf41f560f866e
Sagar Nayak <nayaksagar9080@gmail.com>
2026-08-04 17:52:30 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -494,45 +494,44 @@ def export_excel():
         cell.font = header_font
         cell.alignment = center
 
-   for student in students:
+    for student in students:
+        marks = [
+            m for m in [
+                student[2],
+                student[3],
+                student[4]
+            ]
+            if m is not None
+        ]
 
-    marks = [
-        m for m in [
+        average = round(sum(marks) / len(marks), 2) if marks else 0
+
+        if average >= 90:
+            grade = "A1"
+        elif average >= 80:
+            grade = "A2"
+        elif average >= 70:
+            grade = "B1"
+        elif average >= 60:
+            grade = "B2"
+        elif average >= 50:
+            grade = "C1"
+        elif average >= 40:
+            grade = "C2"
+        elif average >= 33:
+            grade = "D"
+        else:
+            grade = "F"
+
+        sheet.append([
+            student[0],
+            student[1],
             student[2],
             student[3],
-            student[4]
-        ]
-        if m is not None
-    ]
-
-    average = round(sum(marks) / len(marks), 2) if marks else 0
-
-    if average >= 90:
-        grade = "A1"
-    elif average >= 80:
-        grade = "A2"
-    elif average >= 70:
-        grade = "B1"
-    elif average >= 60:
-        grade = "B2"
-    elif average >= 50:
-        grade = "C1"
-    elif average >= 40:
-        grade = "C2"
-    elif average >= 33:
-        grade = "D"
-    else:
-        grade = "F"
-
-    sheet.append([
-        student[0],
-        student[1],
-        student[2],
-        student[3],
-        student[4],
-        average,
-        grade
-    ])
+            student[4],
+            average,
+            grade
+        ])
 
     for column_cells in sheet.columns:
         max_length = 0
@@ -543,16 +542,15 @@ def export_excel():
                 max_length = max(max_length, len(str(cell.value)))
 
         sheet.column_dimensions[column_letter].width = max_length + 5
-  
+
     sheet.freeze_panes = "A2"
-    
+
     sheet.auto_filter.ref = sheet.dimensions
-    
+
     left = Alignment(horizontal="left")
     center = Alignment(horizontal="center")
-    
-    for row in sheet.iter_rows(min_row=2):
 
+    for row in sheet.iter_rows(min_row=2):
         row[0].alignment = center
         row[1].alignment = left
         row[2].alignment = center
